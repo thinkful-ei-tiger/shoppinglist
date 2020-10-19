0bfffafce2913428eed783f8867a26e93cbafd00
James Crandall <jameshowardcrandall@gmail.com>
2020-10-19 09:14:28 -0400

    done

---
+++ index.js
@@ -0,0 +1,42 @@
+function shoppingList() {
+  
+  
+    $('#js-shopping-list-form').submit(function(event){
+      event.preventDefault();
+      
+      const listItem = $('#shopping-list-entry').val();
+      
+      
+     if (listItem !== "") {
+        $('.shopping-list').append(`<li>
+          <span class="shopping-item">${listItem}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>
+        `);
+        $('.js-shopping-list-entry').val("");
+    }
+  });
+  
+  
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
+  });
+  
+  
+  
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+  }
+   
+  
+  
+  
+  $(shoppingList);
